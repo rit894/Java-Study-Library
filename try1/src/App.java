0c9a0b2527f1efe19f2cfd4289d4b8c968cba845
rit894 <nagarithwikv@gmail.com>
2026-01-08 14:23:52 +0530

    Add Variables class with examples of Java variable types and their usage

--- try1/src/App.java
+++ try1/src/App.java
@@ -1,5 +1,35 @@
+//the name of the file must match with the public class name in the code
 public class App{
     public static void main(String[] args) {
         System.out.println("hello world");
     }
 }
+// This is the single line commenting format in java
+
+/* this
+is a
+multiline commenting format in java */
+
+// JAVA VARIABLES
+// there are five types of variables in java such as:
+/* int, float, char, boolean, String */
+class Variables {
+      public static void main(String[] args) {
+        // Variable declaration and initialization
+        int myNum = 5;               // Integer (whole number)
+        float myFloatNum = 5.99f;   // Floating point number
+        char myLetter = 'D';        // Character
+        boolean myBool = true;      // Boolean
+        String myText = "Hello";    // String
+        double myDoubleNum  = 12345.6789; // Double precision floating point number 
+        // Print variables
+        System.out.println("Integer: " + myNum);
+        System.out.println("Float: " + myFloatNum);
+        System.out.println("Character: " + myLetter);
+        System.out.println("Boolean: " + myBool);
+        System.out.println("String: " + myText);
+        System.out.println("Double: " + myDoubleNum);
+    }
+}
+
+
