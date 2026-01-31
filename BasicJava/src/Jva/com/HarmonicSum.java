3fd80e0870572c767c229ce38b3c3bdb0b0e26a9
rit894 <nagarithwikv@gmail.com>
2026-01-31 08:01:14 +0530

    Add HarmonicSum class to calculate and display harmonic sums

---
+++ BasicJava/src/Jva/com/HarmonicSum.java
@@ -0,0 +1,24 @@
+package Jva.com;
+
+public class HarmonicSum {
+public static void main(String[] args) {
+    int max_denominator=20;
+    double harmonic_sum2=0.0;
+    double harmonic_sum1=0.00d;
+    for (int i=1;i<=max_denominator;i++){
+        harmonic_sum1+= 1.0/i;
+    }System.out.println("Harmonic Sum 1"+harmonic_sum1);
+    for (int j=max_denominator ; j>=1;j-- ){
+        if (j==0){
+            break;}
+            else{
+        
+        harmonic_sum2+=1.0/j;}
+    }System.out.println("Harmonic Sum2 "+harmonic_sum2);
+    if (harmonic_sum1!=harmonic_sum2){
+        System.out.println("The Computer has low precssionn and accuracy ");
+    }else{
+        System.out.println("THe Computer is perfectly fine");
+    }
+} 
+}
