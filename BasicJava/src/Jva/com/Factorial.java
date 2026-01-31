e188c6260d22412d65c962a222b365266ded7cd6
rit894 <nagarithwikv@gmail.com>
2026-01-31 08:01:42 +0530

    Remove unnecessary variable declaration in Factorial class

--- BasicJava/src/Jva/com/Factorial.java
+++ BasicJava/src/Jva/com/Factorial.java
@@ -3,7 +3,7 @@ public class Factorial {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         var product =1;
-        var lowerbound = 1;
+        
         var upperbound = 11;
         for (int i=1; i<upperbound ; i++){
             product*=i;
